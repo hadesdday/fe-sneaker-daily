bb120b7b7157d418804850ae1f995a08435226ec
Hieu Nguyen <nhocproaoe@gmail.com>
2023-07-14 17:18:29 +0700

    component: add filter component in product list page

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -1,5 +1,8 @@
+import { Box } from '@mui/material';
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
+import ProductListDesktop from './Desktop';
+import ProductListMobile from './Mobile';
 
 function ProductList(props) {
     let [searchParams, setSearchParams] = useSearchParams();
@@ -8,11 +11,69 @@ function ProductList(props) {
         category: searchParams.get("category"),
         attribute: searchParams.get("attribute"),
     }
+
+    const fakeProducts = [
+        {
+            id: 1,
+            name: "Shoes 1",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+        {
+            id: 2,
+            name: "Shoes 2",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+        {
+            id: 3,
+            name: "Shoes 3",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+        {
+            id: 4,
+            name: "Shoes 4",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+        {
+            id: 5,
+            name: "Shoes 5",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+        {
+            id: 2,
+            name: "Shoes 2",
+            color: "Navy Blue",
+            price: 1190000,
+            imageUrl: "https://ananas.vn/wp-content/uploads/Pro_A6T014_2-500x500.jpeg"
+        },
+    ]
+
+    const filterOptions = {
+        gender: ["all"],
+        category: ["accessories", "shoes"],
+        status: ["limited-edition", "online-only"],
+        style: ["low-top", "slip-on"],
+        productLine: ["basas", "vintas"],
+        price: ["500-599", ">600"],
+        collection: ["ananas-puppet-club", "track-6-2-blues"],
+        material: ["canvas", "cotton"],
+        color: ["white", "pink"]
+    };
+
     return (
-        <div>
-            product list
-            {JSON.stringify(params)}
-        </div>
+        <Box>
+            <ProductListDesktop products={fakeProducts} options={filterOptions} />
+            <ProductListMobile products={fakeProducts} options={filterOptions} />
+        </Box>
     );
 }
 
