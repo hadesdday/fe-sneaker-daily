933bf6f6255cebaa8f601332d56c9ea054500a96
Hieu Nguyen <nhocproaoe@gmail.com>
2023-08-18 16:56:37 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
+import App from './App.jsx'
 import { persistor, store } from './store/store.js'
-import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './utils/theme/index.js'
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
-          <Analytics />
         </BrowserRouter>
       </PersistGate>
     </Provider>
